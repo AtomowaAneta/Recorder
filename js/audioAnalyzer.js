ebc40bbd23dec9169bf8af5f4427e676587d41e1
Stycz <marstyczen@gmail.com>
2017-03-23 12:48:24 +0100

    audioAnaluzer.js file OK

--- js/audioAnalyzer.js
+++ js/audioAnalyzer.js
@@ -1,133 +1,51 @@
-function blobSaver(audioURL) {
-    var recordingList = document.getElementsByTagName("audio");
-    var recordingToDownload = document.createElement("a");
-    recordingToDownload.download = "record.mp3";
-    recordingToDownload.href = audioURL;
-    recordingToDownload.click();
-}function initialize() {
-    var record = document.querySelector(".record");
-    var pause = document.querySelector(".pause");
-    var stop = document.querySelector(".stop");
-    var download = document.querySelector(".download");
-    var chunks = [];
-    var source;
+function analyzeAudio(bufferLength, analyser) {
 
-    var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-    var analyser = audioCtx.createAnalyser();
-    analyser.fftSize = 2048;
+    function draw() {
+        var visualization = document.getElementById("visualization");
+        var canvasCtx = visualization.getContext('2d');
+        var WIDTH = visualization.width;
+        var HEIGHT = visualization.height;
+        drawVisual = requestAnimationFrame(draw);
+        var dataArray = new Uint8Array(bufferLength);
+        analyser.getByteTimeDomainData(dataArray);
 
-    var bufferLength = analyser.fftSize;
+        canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
+        canvasCtx.lineWidth = 2;
+        canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
 
+        canvasCtx.beginPath();
 
+        var sliceWidth = WIDTH * 1.0 / bufferLength;
+        var x = 0;
 
-    if (navigator.getUserMedia || navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia || navigator.msGetUserMedia) {
-        var constraints = {
-            audio: true
-        };
+        for (var i = 0; i < bufferLength; i++) {
 
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then(function(stream) {
-                var mediaRecorder = new MediaRecorder(stream);
+            var v = dataArray[i] / 128.0;
+            var y = v * HEIGHT / 2;
 
-                source = audioCtx.createMediaStreamSource(stream);
-                source.connect(analyser);
+            if (i === 0) {
+                canvasCtx.moveTo(x, y);
+            } else {
+                canvasCtx.lineTo(x, y);
+            }
 
-                console.log("Ready to record");
-                console.log(mediaRecorder.state);
+            x += sliceWidth;
+        }
 
-                record.onclick = function() {
-                    timeWatch();
-                    mediaRecorder.start();
-                    analyzeAudio(bufferLength, analyser);
-                    console.log("recording in progress");
-                    console.log(mediaRecorder.state);
+        canvasCtx.lineTo(visualization.width, visualization.height / 2);
+        canvasCtx.stroke();
+    };
+    draw();
+}
 
-                }
-
-                pause.onclick = function() {
-
-                }
-
-                stop.onclick = function() {
-                    freezeTime();
-                    stopVisualization();
-                    mediaRecorder.stop();
-                    //  console.log("recording stopped");
-                    console.log(mediaRecorder.state);
-                }
-
-                mediaRecorder.ondataavailable = function(e) {
-                    chunks.push(e.data);
-                }
-
-                mediaRecorder.onstop = function(e) {
-                    console.log("recording stopped");
-                    var clipRow = document.createElement('div');
-                    var cols = document.createElement('div');
-                    var cols1 = document.createElement('div');
-
-                    var audio = document.createElement('audio');
-                    var deleteButton = document.createElement('button');
-                    var downloadButton = document.createElement('button');
-
-                    deleteButton.classList.add("btn");
-                    deleteButton.classList.add("btn-warning");
-                    downloadButton.classList.add("btn");
-                    downloadButton.classList.add("btn-info");
-                    cols.classList.add("col-md-10");
-
-
-                    cols1.classList.add("col-md-2");
-                    cols1.classList.add("butt");
-                    clipRow.classList.add("row");
-                    clipRow.id = "record";
-                    audio.setAttribute('controls', '');
-                    deleteButton.innerHTML = "Delete";
-                    downloadButton.innerHTML = "Download";
-
-                    clipRow.appendChild(cols);
-                    cols.appendChild(audio);
-
-                    clipRow.appendChild(cols1);
-                    cols1.appendChild(deleteButton);
-                    cols1.appendChild(downloadButton);
-                    recordings.appendChild(clipRow);
-
-
-
-                    var blob = new Blob(chunks, {
-                        'type': 'audio/mp3; codecs=opus'
-                    });
-                    chunks = [];
-                    var audioURL = window.URL.createObjectURL(blob);
-                    audio.src = audioURL;
-
-                    deleteButton.onclick = function() {
-
-                        var nodeList = document.getElementById("record");
-                        nodeList.parentNode.removeChild(nodeList);
-                        console.log("recording has been removed");
-
-                    }
-
-                    downloadButton.onclick = function() {
-                        blobSaver(audioURL);
-                        console.log("Audio has been downloaded");
-                    }
-
-                }
-
-            }, printError);
-
-    } else {
-        console.log("Not supported");
-
-    }
-
-    function printError(e) {
-        console.log('Failed', e);
-    }
-
+function stopVisualization() {
+    var visualization = document.getElementById("visualization");
+    var canvasCtx = visualization.getContext('2d');
+    var WIDTH = visualization.width;
+    var HEIGHT = visualization.height;
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+    canvasCtx.fillStyle = "red";
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 }
